42ea57963f385351531d38b8fdc9e32872f3db61
Andres Gutierrez <jm8115692@gmail.com>
2026-07-17 13:25:13 -0500

    feat(matches): render my-matches list

---
+++ assets/js/matches.js
@@ -0,0 +1,21 @@
+import { api } from './api.js';
+import { $, esc, cop, initials, formatDate, formatTime, formatLabel } from './helpers.js';
+import { state, nav } from './state.js';
+import { go } from './router.js';
+import { toast } from './toast.js';
+import { loadFeed } from './home.js';
+import { cardHTML, emptyHTML, missingChip } from './home.js';
+import { openChat } from './chat.js';
+
+export async function renderMyMatches() {
+  const el = $('my-matches-feed');
+  el.innerHTML = '<div class="skel"></div><div class="skel"></div>';
+  try {
+    const r = await api('partidos.php?mios=1');
+    el.innerHTML = r.partidos.length
+      ? r.partidos.map((p) => cardHTML(p, 'my-matches')).join('')
+      : emptyHTML('⚽', "You don't have any matches yet", 'Join one from Home and it will show up here');
+  } catch (e) {
+    el.innerHTML = emptyHTML('📡', 'Error loading', e.message);
+  }
+}
